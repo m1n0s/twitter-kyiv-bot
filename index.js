df25a02d105a12f4437a6dac9e8deaca02c4acac
m1n0s <leontiy1992@gmail.com>
2017-12-23 12:56:39 +0200

    Add saving to MongoDB every new liked tweet.

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
 const Twit = require('twit');
+const mongoose = require('mongoose');
 
 // Upload .env variables into process.env
 require('dotenv').config();
 
 const {
-    CONSUMER_KEY,
-    CONSUMER_SECRET,
-    ACCESS_TOKEN,
-    ACCESS_TOKEN_SECRET,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET,
+  MONGO_CONNECT,
 } = process.env;
 
-const bot = new Twit({
-    consumer_key: CONSUMER_KEY,
-    consumer_secret: CONSUMER_SECRET,
-    access_token: ACCESS_TOKEN,
-    access_token_secret: ACCESS_TOKEN_SECRET,
-    timeout_ms: 60*1000,
-})
+mongoose.connect(MONGO_CONNECT, { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
-const likeTweet = (id) => {
-    bot.post('favorites/create', { id }).then((res) => console.log(res.data.errors));
+const db = mongoose.connection;
+const Tweet = mongoose.model('Tweet', { data: Object });
+
+db.once('open', () => console.log('connection to DB established'));
+
+const bot = new Twit({
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
+});
+
+const likeTweet = (tweet) => {
+  const { id_str: id } = tweet;
+
+  bot.post('favorites/create', { id }).then((res) => {
+    const { errors } = res.data;
+
+    if (errors) {
+      console.log(errors);
+    } else {
+      const tweetRecord = new Tweet({ data: tweet });
+
+      tweetRecord.save().then(() => console.log('tweet saved'));
+    }
+  });
 };
 
 const stream = bot.stream('statuses/filter', { track: ['Kyiv', 'Kiev'].join() });
 const re = /\b(Kyiv|Kiev)\b/i;
 
 stream.on('tweet', (tweet) => {
-    const { text = '', extended_tweet } = tweet;
-    let fullText;
+  const { text = '', extended_tweet } = tweet;
+  let fullText;
 
-    if (extended_tweet) {
-        fullText = extended_tweet.full_text || '';
-    }
+  if (extended_tweet) {
+    fullText = extended_tweet.full_text || '';
+  }
 
-    if (re.test(text) || re.test(fullText)) {
-        likeTweet(tweet.id_str);
-    }
+  if (re.test(text) || re.test(fullText)) {
+    likeTweet(tweet);
+  }
 });
